d8a87aca43f3984c560114f518006bf07f675cad
Randall Mora <randall.mora@rossmon.co.cr>
2024-10-09 09:04:17 -0600

    agregar metodos  para compatibilidad de conversion de archivo a pdf  cuando son doc y rtf

--- src/components/visorArchivos/visorArchivos.tsx
+++ src/components/visorArchivos/visorArchivos.tsx
@@ -3,11 +3,18 @@ import { Button } from "react-bootstrap";
 import FileViewer from "react-file-viewer";
 import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 import { AiOutlineClose } from "react-icons/ai";
+import { ObtenerArchivoDocumento } from "../../servicios/ServicioDocumentos";
+import { useSpinner } from "../../context/spinnerContext";
 
-export const VisorArchivos: React.FC<any> = ({ documento, cerrar }) => {
+export const VisorArchivos: React.FC<any> = ({
+  documento,
+  documentoDescarga,
+  cerrar,
+}) => {
   const [error, setError] = useState("");
-  const [fileExtension, setFileExtension] = useState<any>("");
-  const [fileURL, setFileURL] = useState<any>("");
+  const [fileExtension, setFileExtension] = useState<any>(null);
+  const [fileURL, setFileURL] = useState<any>(null);
+  const { setShowSpinner } = useSpinner();
 
   const tiposSoportados = [
     "docx",
@@ -22,6 +29,33 @@ export const VisorArchivos: React.FC<any> = ({ documento, cerrar }) => {
     "bmp",
   ].join(",");
 
+  const obtenerArchivo = async (ids: any) => {
+    setShowSpinner(true);
+    const archivoBlob = await ObtenerArchivoDocumento(ids);
+    const url = URL.createObjectURL(archivoBlob);
+    setShowSpinner(false);
+    setFileURL(url);
+    let extensionArchivo = documentoDescarga.nombre.split(".").pop();
+    if (extensionArchivo === "doc") {
+      extensionArchivo = "pdf";
+    }
+    setFileExtension(extensionArchivo);
+  };
+
+  useEffect(() => {
+    if (documentoDescarga) {
+      setTimeout(() => {
+        obtenerArchivo([documentoDescarga.idDocumento]);
+      }, 5);
+    }
+    return () => {
+      if (fileURL) {
+        URL.revokeObjectURL(fileURL); // Revoca el Object URL creado
+        setFileURL(null); // Limpia el estado del URL
+      }
+    };
+  }, [documentoDescarga]);
+
   useEffect(() => {
     if (documento) {
       setFileURL("");
@@ -34,52 +68,66 @@ export const VisorArchivos: React.FC<any> = ({ documento, cerrar }) => {
         setFileExtension(extension);
       }, 5); // Un retraso mínimo de 5ms para forzar el refresco
     }
+    // Función de limpieza para liberar la memoria cuando el componente se desmonta
+    return () => {
+      if (fileURL) {
+        URL.revokeObjectURL(fileURL); // Revoca el Object URL creado
+        setFileURL(null); // Limpia el estado del URL
+      }
+    };
   }, [documento]);
 
   if (tiposSoportados.includes(fileExtension)) {
     return (
       <>
-        <div>
-          <div className="mb-2 d-flex justify-content-between align-items-center">
-            <h4 className="mb-0">
-              Visualización del archivo: {documento.name}
-            </h4>
-            <Button className="btn-cancel" onClick={() => cerrar()}>
-              <AiOutlineClose />
-            </Button>
+        {fileURL && (
+          <div style={{display:"block",maxHeight:"200px"}}>
+            <div className="mb-2 d-flex justify-content-between align-items-center">
+              <h4 className="mb-0">
+                {documento?.name || documentoDescarga?.nombre}
+              </h4>
+              <Button className="btn-cancel" onClick={() => cerrar()}>
+                <AiOutlineClose />
+              </Button>
+            </div>
+
+            {error && <p>No se ha podido mostrar el archivo</p>}
+
+            <FileViewer
+              key={documento?.id || documentoDescarga?.idDocumento}
+              fileType={fileExtension}
+              filePath={fileURL}
+              errorComponent={<div>Error al cargar el archivo</div>} // Error personalizado
+              onError={() => setError("error")}
+            />
           </div>
-
-          {error && <p>No se ha podido mostrar el archivo</p>}
-
-          <FileViewer
-            fileType={fileExtension}
-            filePath={fileURL}
-            errorComponent={<div>Error al cargar el archivo</div>} // Error personalizado
-            onError={() => setError("error")}
-          />
-        </div>{" "}
+        )}
       </>
     );
   } else {
     return (
       <>
         <div className="mb-2 d-flex justify-content-between align-items-center">
-          <h4 className="mb-0">Visualización del archivo: {documento.name}</h4>
+          <h4 className="mb-0">
+            {documento?.name || documentoDescarga?.nombre}
+          </h4>
           <Button className="btn-cancel" onClick={() => cerrar()}>
             <AiOutlineClose />
           </Button>
         </div>
-        {!["html", "sql","txt"].includes(fileExtension) && (
+        {["html", "sql", "txt"].includes(fileExtension) && (
           <p style={{ color: "#9E0000" }}>
             El archivo ha sido descargado ya que posee un formato no soportado.
           </p>
         )}
-        <iframe
-          src={fileURL}
-          width="100%"
-          height="600px"
-          title="File Preview"
-        />
+        {fileURL && (
+          <iframe
+            src={fileURL}
+            width="100%"
+            height="600px"
+            title="File Preview"
+          />
+        )}
       </>
     );
   }
--- src/servicios/ServicioDocumentos.ts
+++ src/servicios/ServicioDocumentos.ts
@@ -4,12 +4,17 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API_BASE_URL_CARGA = import.meta.env.VITE_API_BASE_URL_CARGA;
 const API_BASE_URL_DESCARGA = import.meta.env.VITE_API_BASE_URL_DESCARGA;
 
-export const ObtenerDocumentosPorContenido= async (data: any) => {
-    const url = `${API_BASE_URL_DESCARGA}/${controlador}/ObtenerDocumentosPorContenido`;
-    return await ProcesarDatosApi('POST', url, data);
-}
+export const ObtenerDocumentosPorContenido = async (data: any) => {
+  const url = `${API_BASE_URL_DESCARGA}/${controlador}/ObtenerDocumentosPorContenido`;
+  return await ProcesarDatosApi("POST", url, data);
+};
 
-export const ObtenerDocumento= async (data: any) => {
-    const url = `${API_BASE_URL}/${controlador}/ObtenerDocumento`;
-    return await ProcesarDatosApi('POST', url, data);
-}
+export const ObtenerArchivoDocumento = async (data: any) => {
+  const url = `${API_BASE_URL_DESCARGA}/${controlador}/ObtenerDocumentos`;
+  return await ProcesarDatosApi("POST", url, data, true);
+};
+
+export const ObtenerDocumento = async (data: any) => {
+  const url = `${API_BASE_URL}/${controlador}/ObtenerDocumento`;
+  return await ProcesarDatosApi("POST", url, data);
+};
